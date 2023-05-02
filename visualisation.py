3268dda2a093a2d17790a0d5106d20320a21ec4f
Stanisław Maliński <95365847+Stanleyowskiki@users.noreply.github.com>
2023-05-02 17:37:29 +0200

    Added visualisation for a folder

--- visualisation.py
+++ visualisation.py
@@ -1,3 +1,5 @@
+import os
+import re
 import pandas as pd
 import matplotlib.pyplot as plt
 from util import terms
@@ -10,10 +12,21 @@ def visualise(data: pd.DataFrame, title="DNN reaserch"):
     plt.title(title)
     plt.show()
 
-def visualise_dic(data :dict):
-    for key in data.keys():
-        visualise(data[key], terms[key])
+def visualise_folder(path):
+    if not os.path.isdir(path):
+        Exception("Podany folder nie istnieje.")
+
+    files = [path + "\\" + f for f in os.listdir(path)]
+
+    for file in files:
+        if os.path.isdir(file):
+            files += [file + "\\" + f for f in os.listdir(file)]
+        else:
+            env = re.findall(r'\\([\w\s]*)\\(\w+).csv', file)[0]
+            df = pd.read_csv(file).drop(columns=['Unnamed: 0'])
+            visualise(df, str(env[0]) + " - " + terms[str(env[1])])
+
 
 if __name__ == "__main__":
-    df = pd.read_csv("C:\\Users\\Staszek\\Documents\\Investing\\DNNReasearch\\Results\\Results(04-24-23)\\arra.txt").drop(columns=['Unnamed: 0'])
-    visualise(df)
+    dir = "/Results/Results(05-02-23(1))"
+    visualise_folder(dir)
