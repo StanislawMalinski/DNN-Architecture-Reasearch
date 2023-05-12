292f42b796ba613ec9c2554c349a1d24d5b594be
Stanleyowskiki <s.t.malinski@gmail.com>
2023-05-12 22:06:20 +0200

    visualisation -> added new function for visualisation that allows limit number of graphs on the figure.

--- visualisation.py
+++ visualisation.py
@@ -4,29 +4,56 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from util import terms
 
+BACK_SLASH = False
 
 def visualise(data: pd.DataFrame, title="DNN reaserch"):
     data.plot()
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.xlabel('Epoch')
+    plt.ylabel('RMS')
     plt.title(title)
     plt.show()
 
-def visualise_folder(path):
+def visualise_folder(path, worst=-1, best=-1):
     if not os.path.isdir(path):
         Exception("Podany folder nie istnieje.")
+    if BACK_SLASH:
+        files = [path + "\\" + f for f in os.listdir(path)]
+        for file in files:
+            if os.path.isdir(file):
+                files += [file + "\\" + f for f in os.listdir(file)]
+            else:
+                env = re.findall(r'\\([\w\s]*)\\(\w+).csv', file)[0]
+                df = pd.read_csv(file).drop(columns=['Unnamed: 0'])
+                __show_figure(df, env, best, worst)
+    else:
+        files = [path + "/" + f for f in os.listdir(path)]
+        for file in files:
+            if os.path.isdir(file):
+                files += [file + "/" + f for f in os.listdir(file)]
+            else:
+                env = re.findall(r'/([\w\s]*)/(\w+).csv', file)[0]
+                df = pd.read_csv(file).drop(columns=['Unnamed: 0'])
+                __show_figure(df, env, best, worst)
 
-    files = [path + "\\" + f for f in os.listdir(path)]
 
-    for file in files:
-        if os.path.isdir(file):
-            files += [file + "\\" + f for f in os.listdir(file)]
+def __show_figure(df, names, best, worst):
+    s = df.sum().sort_values()
+    label = []
+    if best > 0:
+        bs = s[:best]
+        label += [name for name in bs.index]
+        if worst > 0:
+            ws = s[-worst:]
+            label += [name for name in ws.index]
+    else:
+        if worst > 0:
+            ws = s[-worst:]
+            label += [name for name in ws.index]
         else:
-            env = re.findall(r'\\([\w\s]*)\\(\w+).csv', file)[0]
-            df = pd.read_csv(file).drop(columns=['Unnamed: 0'])
-            visualise(df, str(env[0]) + " - " + terms[str(env[1])])
+            label = s.index
 
+    visualise(df[label], str(names[0]) + " - " + terms[str(names[1])])
 
 if __name__ == "__main__":
-    dir = "/Results/Results(05-02-23(1))"
-    visualise_folder(dir)
+    dir = "/Users/stanislaw/PycharmProjects/DNNreasearch/Results/Results(05-03-23)"
+    visualise_folder(dir,worst=1,best=2 )
